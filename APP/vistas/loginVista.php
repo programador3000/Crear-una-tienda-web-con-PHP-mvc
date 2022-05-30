1d4a65231ec082a6a98dfb4feefa323b9829cbb9
cristobal <cristobal.paez.pd@gmail.com>
2022-05-29 20:54:55 -0500

    Crear el formulario del login con Bootstrap 4

--- APP/vistas/loginVista.php
+++ APP/vistas/loginVista.php
@@ -17,22 +17,28 @@
         <div class="row content">
             <div class="col-sm-2"></div>
             <div class="col-sm-8">
-    <form>
-    <table>
-        <tr>
-            <td>Usuario:</td>
-            <td><input type="text" name="usuario"></td>
-        </tr>
-        <tr>
-            <td>Clave acceso:</td>
-            <td><input type="password" name="clave"></td>
-        </tr>
-        <tr>
-            <td>&nbsp;</td>
-            <td><input type="submit" value="Enviar"></td>
-        </tr>
-    </table>
+                <h1 class="text-center">Login</h1>
+                <div class="card p-4 bg-light">
+    <form action="login/verifica/" method="POST">
+        <div class="form-grou text-left">
+            <label for="Usuario">Usuario:</label>
+            <input type="text" name="usuario" class="form-control">
+        </div>
+        <div class="form-group text-left">
+            <label for="clave">Clave acceso:</label> 
+            <input type="password" name="clave" class="form-control">
+        </div>
+        <div class="form-group text-left">
+            <input type="submit" value="Enviar" class="btn btn-success"></td>
+        </div>
+        
+            <input type="checkbox" name="recordar">
+            <label for="clave" class="">Recordar</label>
+        
     </form>
+    </div><!--card-->
+    <a href="login/alta/" >Darse de alta en el sistema</a><br>
+        <a href="login/olvido/" >¿Olvidaste tu clave de acceso?</a>
     </div><!--8col-->
     <div class="col-sm-2"></div>
     </div><!--row-->
